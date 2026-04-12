27c8cddd6ab5ff64cbc0c8a3766fb7165f58ca73
idevRavn <idevravn@gmail.com>
2026-04-12 21:40:39 +0530

    add phonebook step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const changeName = (event) => {
     setNewName(event.target.value);
+  };
+
+  const changeNumber = (event) => {
+    setNewNumber(event.target.value);
   };
 
   const addPerson = (event) => {
@@ -16,9 +26,12 @@ const App = () => {
     }
     const personObject = {
       name: newName,
+      number: newNumber,
+      id: persons.length + 1,
     };
     setPersons(persons.concat(personObject));
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -27,6 +40,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={changeName} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={changeNumber} />
         </div>
         <div>
           <button type="submit">add</button>
