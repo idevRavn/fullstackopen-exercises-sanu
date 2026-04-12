88dd2318b3e7116759e4dd69a1c4bd8d91977614
idevRavn <idevravn@gmail.com>
2026-04-12 12:12:13 +0530

    add unicafe 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,20 +1,32 @@
 import { useState } from "react";
 
+const StatisticsLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
 const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
 
 const Statistics = ({ good, neutral, bad }) => {
-  if (good + neutral + bad === 0) {
+  const total = good + neutral + bad;
+  const average = ((good - bad) / total).toFixed(1);
+  const positive = ((good / total) * 100).toFixed(1) + "%";
+  if (total === 0) {
     return <p>No feedback given</p>;
   }
   return (
-    <>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>total: {good + neutral + bad}</p>
-      <p>average: {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive: {(good / (good + neutral + bad)) * 100} %</p>
-    </>
+    <table>
+      <tbody>
+        <StatisticsLine text="good" value={good} />
+        <StatisticsLine text="neutral" value={neutral} />
+        <StatisticsLine text="bad" value={bad} />
+        <StatisticsLine text="total" value={total} />
+        <StatisticsLine text="average" value={average} />
+        <StatisticsLine text="positive" value={positive} />
+      </tbody>
+    </table>
   );
 };
 
