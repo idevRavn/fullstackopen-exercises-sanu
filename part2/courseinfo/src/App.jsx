ee235e5c4393eb5612f2baecdff2f2226ca9c01c
idevRavn <idevravn@gmail.com>
2026-04-12 19:10:02 +0530

    add part2 courseinfo step 6

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,64 @@
+const Header = ({ text }) => {
+  return <h1>{text}</h1>;
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => {
+        return (
+          <Part
+            key={part.id}
+            name={part.name}
+            exercises={part.exercises}
+          ></Part>
+        );
+      })}
+    </>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header text={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
