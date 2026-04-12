6a32dc6a02651101264077069bb9428142123712
idevRavn <idevravn@gmail.com>
2026-04-12 19:44:46 +0530

    add total using reduce courseinfo step 7-8

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -26,11 +26,17 @@ const Content = ({ parts }) => {
   );
 };
 
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Number of exercises {total}</p>;
+};
+
 const Course = ({ course }) => {
   return (
     <>
       <Header text={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   );
 };
